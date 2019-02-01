5cd826a34cc429276ccc31461414617188a9c311
Abhay Diwan <diwanabh@Corp.FC.LOCAL>
2019-02-01 14:33:31 -0500

    created client for sum

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -5,36 +5,34 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/abhaydiwan/grpc/greet/greetpb"
+	"github.com/abhaydiwan/grpc/calculator/calculatorpb"
 	"google.golang.org/grpc"
 )
 
 func main() {
 
-	fmt.Println("Hello I'm a client")
+	fmt.Println("Calcualtor client")
 	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Could not connect %v", err)
 	}
 	defer cc.Close()
 
-	c := greetpb.NewGreetServiceClient(cc)
+	c := calculatorpb.NewCalculatorServiceClient(cc)
 	//fmt.Println("Created client: %f", c)
 	doUnary(c)
 }
 
-func doUnary(c greetpb.GreetServiceClient) {
-	fmt.Println("Starting to do a Unary RPC...")
-	req := &greetpb.GreetRequest{
-		Greeting: &greetpb.Greeting{
-			FirstName: "Abhay",
-			LastName:  "Diwan",
-		},
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a Sum Unary RPC...")
+	req := &calculatorpb.SumRequest{
+		FirstNumber:  20,
+		SecondNumber: 30,
 	}
 
-	res, err := c.Greet(context.Background(), req)
+	res, err := c.Sum(context.Background(), req)
 	if err != nil {
-		log.Fatalf("error while calling Greet RPC: %v", err)
+		log.Fatalf("error while calling Sum RPC: %v", err)
 	}
-	log.Printf("Response from Greet: %v", res.Result)
+	log.Printf("Response from Greet: %v", res.SumResult)
 }
